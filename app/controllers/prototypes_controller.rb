8e8a54c244a06c3891b2c2650e7684eb547e0a52
baku_nishiya <niya-baku@users.noreply.github.com>
2018-08-09 01:40:48 +0900

    Merge branch 'master' into copy
    # Conflicts:
    #	app/assets/stylesheets/prototypes.scss
    #	app/controllers/prototypes_controller.rb
    #	app/uploaders/thumbnail_uploader.rb
    #	config/routes.rb
    #	db/schema.rb
    #	test/fixtures/prototypes.yml
    #	test/fixtures/thumbnails.yml

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,18 +1,11 @@
 class PrototypesController < ApplicationController
   def new
-    @prototype = Prototype.new
-    3.times { @prototype.thumbnails.build }
-  end
-
-  def create
-
-    @prototype = Prototype.create(create_params)
-
-    redirect_to '/'
+    @prototype = Prototype.new(create_params)
+    3.times { @prototype.thumbnails.build }  #ここで投稿の数を調整します。今回は3枚投稿
   end
 
   private
-  def create_params
+    def create_params
     params.require(:prototype).permit(thumbnails_attributes: [:image])
   end
 end
